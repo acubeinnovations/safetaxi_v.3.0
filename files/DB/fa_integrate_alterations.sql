b9b40caf82b58ce860f5421b9784df50436e6dd6
rosyswapna <rosy.swapna@gmail.com>
2015-04-29 16:13:36 +0530

    add customer api integration

--- files/DB/fa_integrate_alterations.sql
+++ files/DB/fa_integrate_alterations.sql
@@ -1,3 +1,4 @@
 
 -----------------29/04/2015-------------------------------------------
 ALTER TABLE `trips`  ADD `invoice_no` INT(11) NOT NULL AFTER `remarks`;
+ALTER TABLE `customers`  ADD `fa_customer_id` INT(11) NOT NULL AFTER `customer_status_id`;
